36f2dcfe3210b5975924ebc01d33fb3f2979d680
Nikvitya <nikvitya@yandex.ru>
2025-06-27 17:46:05 +0300

    Revert "Feature/calculator: add controller, services, logging, tests, swagger."
    This reverts commit d6961328bc81c046e56404a30349f8d01e8da5c3.

---
+++ src/test/java/ru/neoflex/neoflex_credit_bank/NeoflexCreditBankApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.neoflex.neoflex_credit_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoflexCreditBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
